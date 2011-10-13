33e4d126522b8bde4e01af22866bff1323f1e2ca
mjota <manuel.pol@gmail.com>
2011-10-13 10:40:13 +0200

    Añadidido ficheros_basicos.c

---
+++ src/ficheros_basico.c
@@ -0,0 +1,14 @@
+/*
+ * ficheros_basico.h
+ *
+ *  Created on: 11/10/2011
+ *      Author: manuel
+ */
+
+// fichero ficheros_basico.h
+
+int tamMB (unsigned int nbloques);
+int tamAI (unsigned int ninodos);
+int initSB(unsigned int nbloques, unsigned int ninodos);
+int initMB(unsigned int nbloques);
+int initAI(unsigned int ninodos);
