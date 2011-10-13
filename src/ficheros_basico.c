d1e9f99dac605c768a9220a0de8d61a0a917eda4
mjota <manuel.pol@gmail.com>
2011-10-13 11:18:41 +0200

    Añadidos tamMB y tamAI

--- src/ficheros_basico.c
+++ src/ficheros_basico.c
@@ -7,8 +7,28 @@
 
 // fichero ficheros_basico.h
 
-int tamMB (unsigned int nbloques);
-int tamAI (unsigned int ninodos);
+#include "ficheros_basico.h"
+
+int tamMB (unsigned int nbloques){
+	if (nbloques % (blocksize*8)==0){
+		return nbloques/(blocksize*8);
+	}else{
+		return (nbloques/(blocksize*8))+1;
+	}
+}
+
+
+int tamAI (unsigned int ninodos){
+	int block;
+	block = blocksize/4;
+	if (((ninodos*128)%block)==0){
+		return (ninodos*128)/block;
+	}else{
+		return ((ninodos*128)/block)+1;
+	}
+}
+
+
 int initSB(unsigned int nbloques, unsigned int ninodos);
 int initMB(unsigned int nbloques);
 int initAI(unsigned int ninodos);
